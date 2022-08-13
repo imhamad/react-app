815b60bf7d919792369747151a7cf60dbe6bd311
Hamad <hamad.coder@gmail.com>
2022-08-13 16:03:38 +0500

    REACT One Pager and Understanding REACT Detailed concepts

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('test'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <h1>Hello World</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate doloribus ea laboriosam nam, porro
+            possimus sit? Consequatur culpa ipsam itaque modi nesciunt recusandae vel voluptatum. A cumque labore
+            possimus praesentium?</p>
+
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
